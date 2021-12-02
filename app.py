125fdb0574fb7562215eb4606820cb5749cbc985
Ayman Ahmed <eccentricayman@10-18-124-43.dynapool.wireless.nyu.edu>
2021-12-02 15:00:59 -0500

    register

--- app.py
+++ app.py
@@ -19,6 +19,87 @@ init()
 def index():
 	return render_template("index.html")
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+	if request.method == "POST":
+		sourceCity = request.form.get('sourceCity')
+		sourceAirport = request.form.get('sourceAirport')
+		destinationCity = request.form.get('destinationCity')
+		destinationAirport = request.form.get('destinationAirport')
+		departureDate = request.form.get('departureDate')
+		arrivalDate = request.form.get('departureDate')
+		
+	else:
+		return render_template("search.html")
+
+#Define route for register
+@app.route('/register', methods=['GET','POST'])
+def register():
+	if request.method == "POST":
+		#grabs information from the forms
+		username = request.form['username']
+		password = request.form['password']
+
+		cursor = db.cursor()
+		error = None
+
+		customerQuery = 'SELECT * FROM Customer WHERE email = %s'
+		cursor.execute(customerQuery, (username))
+		customerData = cursor.fetchone()
+
+		airlineQuery = 'SELECT * FROM AirlineStaff WHERE username = %s'
+		cursor.execute(airlineQuery, (username))
+		airlineData = cursor.fetchone()
+		
+		if(customerData or airlineData):
+			#If the previous query returns data, then user exists
+			error = "This user already exists"
+			return render_template('register.html', error = error)
+		else:
+			ins = 'INSERT INTO user VALUES(%s, %s)'
+			cursor.execute(ins, (username, password))
+			db.commit()
+			cursor.close()
+			return render_template('index.html')
+	else: 
+		return render_template('register.html')
+
+#Define route for login
+@app.route('/login', methods=['GET','POST'])
+def login():
+	if request.method == "POST":
+		#grabs information from the forms
+		username = request.form['username']
+		password = request.form['password']
+
+		cursor = db.cursor()
+		error = None
+
+		customerQuery = 'SELECT * FROM Customer WHERE email = %s and password = %s'
+		cursor.execute(customerQuery, (username, password))
+		customerData = cursor.fetchone()
+
+		airlineQuery = 'SELECT * FROM AirlineStaff WHERE username = %s and password = %s'
+		cursor.execute(airlineQuery, (username, password))
+		airlineData = cursor.fetchone()
+
+		cursor.close()
+		
+		if (customerData):
+			session['username'] = username
+			session['user'] = 'Customer'
+			return redirect(url_for('home'))
+		elif (airlineData):
+			session['username'] = username
+			session['user'] = 'AirlineStaff'
+			return redirect(url_for('home'))
+		else:
+			error = 'Invalid login or username'
+			return render_template('login.html', error=error)
+	else:
+		return render_template('login.html')
+
+
 
 
 if __name__ == "__main__":
